8c7658749ff8058651d6e054c5cac52be45e6902
Manav Srivastava <manavsrivastav14@gmail.com>
2023-06-19 14:54:36 +0530

    Functional Component In React: Method 1

--- index.js
+++ index.js
@@ -17,20 +17,33 @@
 // ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
 
 // Creating React element with JSX
-const JSXHeading = (
-  //   <React.Fragment>
-  //     <h1>Hello React with JSX!</h1>
-  //     <p>This is a paragraph in JSX</p>
-  //   </React.Fragment>
+// const JSXHeading = (
+//   <React.Fragment>
+//     <h1>Hello React with JSX!</h1>
+//     <p>This is a paragraph in JSX</p>
+//   </React.Fragment>
 
-  <>
-    <h1>About React</h1>
-    <p>React is a javascript library used to build user interfaces.</p>
-    <ul>
-      <li>React is declarative</li>
-      <li>Used to build SPA</li>
-      <li>React is composable</li>
-    </ul>
-  </>
-);
-ReactDOM.createRoot(document.getElementById("root")).render(JSXHeading);
+//   <>
+//     <h1>About React</h1>
+//     <p>React is a javascript library used to build user interfaces.</p>
+//     <ul>
+//       <li>React is declarative</li>
+//       <li>Used to build SPA</li>
+//       <li>React is composable</li>
+//     </ul>
+//   </>
+// );
+function App() {
+  return (
+    <>
+      <h1>React Project </h1>
+      <p>Skills used to make this project</p>
+      <ol>
+        <li>HTML</li>
+        <li>javaScript</li>
+        <li>CSS</li>
+      </ol>
+    </>
+  );
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
