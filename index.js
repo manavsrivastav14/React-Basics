0425a8cbd4053ba9d5522f62409b3e92ec6c39c2
Manav Srivastava <manavsrivastav14@gmail.com>
2023-06-19 14:02:56 +0530

    Using babel to convert JSX to JS. Creating first JSX Element

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const heading = document.createElement("h2");
-heading.textContent = "Hello World Javascript";
-heading.className = "header";
-document.getElementById("root").append(heading);
+// const heading = document.createElement("h2");
+// heading.textContent = "Hello World Javascript";
+// heading.className = "header";
+// document.getElementById("root").append(heading);
 
-console.log("Javascript element:", heading);
+// console.log("Javascript element:", heading);
 
-// Creating React element
-// const React = require("react");
+// Creating React element with JS
 
-const reactHeading = React.createElement("h1", {
-  className: "head",
-  id: "reactHead",
-  children: "Hello React!!",
-});
-console.log("React element:", reactHeading);
+// const reactHeading = React.createElement("h1", {
+//   className: "head",
+//   id: "reactHead",
+//   children: "Hello React!!",
+// });
+// console.log("React element:", reactHeading);
 
-ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+// ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);
+
+// Creating React element with JSX
+const JSXHeading = (
+  <div>
+    <h1>Hello React with JSX!</h1>
+    <p>This is a paragraph in JSX</p>
+  </div>
+);
+ReactDOM.createRoot(document.getElementById("root")).render(JSXHeading);
