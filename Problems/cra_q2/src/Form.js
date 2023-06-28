52691bfa0d68026df8936ae3b348f5e86f5a989b
Manav Srivastava <manavsrivastav14@gmail.com>
2023-06-28 10:30:40 +0530

    Setting up folder structure for problems q2 of create-react-app

---
+++ Problems/cra_q2/src/Form.js
@@ -0,0 +1,9 @@
+// Complete the Form Component and export it
+
+const Form = () => (
+  <>
+    <div>
+      <form>{/* Create a h3, 2 inputs and 1 button here */}</form>
+    </div>
+  </>
+);
---
+++ Problems/cra_q2/src/HomePage.js
@@ -0,0 +1,9 @@
+// Complete the HomePage Component and export it
+
+function HomePage() {
+  return (
+    <div className="Homepage">
+      {/* Create a h1 tag and render Form Component here */}
+    </div>
+  );
+}
