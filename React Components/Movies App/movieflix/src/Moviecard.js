3954388289ac2db7ab8549292044cc3d75964cdf
Manav Srivastava <manavsrivastav14@gmail.com>
2023-06-28 14:19:02 +0530

    Adding stars to the movie

--- React Components/Movies App/movieflix/src/Moviecard.js
+++ React Components/Movies App/movieflix/src/Moviecard.js
@@ -1,7 +1,35 @@
 import { Component } from "react";
 
 class MovieCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "The Avengers",
+      plot: "Supernatural Powers shown in the movie.",
+      price: 199,
+      rating: 8.9,
+      stars: 0,
+    };
+  }
+  addStars = () => {
+    // this.state.stars += 0.5;
+
+    // Form 1 of setState
+    // this.setState({
+    //   stars: this.state.stars + 0.5,
+    // });
+
+    // Form 2 of setState
+    this.setState((prevState) => {
+      return {
+        stars: prevState.stars + 0.5,
+      };
+    });
+  };
+  removeStars() {}
+
   render() {
+    const { title, plot, price, rating, stars } = this.state;
     return (
       <div className="main">
         <div className="movie-card">
@@ -13,17 +41,18 @@ class MovieCard extends Component {
           </div>
 
           <div className="right">
-            <div className="title">The Avengers</div>
-            <div className="plot">Supernatural Powers shown in the movie.</div>
-            <div className="price">Rs. 199</div>
+            <div className="title">{title}</div>
+            <div className="plot">{plot}</div>
+            <div className="price">Rs. {price}</div>
 
             <div className="footer">
-              <div className="rating">8.9</div>
+              <div className="rating">{rating}</div>
               <div className="star-dis">
                 <img
                   src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png"
                   alt="increase-star"
                   className="str-btn"
+                  onClick={this.addStars}
                 />
                 <img
                   src="https://t4.ftcdn.net/jpg/00/61/25/93/240_F_61259351_3EL2WAZWZTXSQb1dPEzWu0yFLZNfIzFC.jpg"
@@ -36,7 +65,7 @@ class MovieCard extends Component {
                   className="str-btn"
                 />
               </div>
-              <span className="starCount">5</span>
+              <span className="starCount">{stars}</span>
               <button className="favourite-btn">Favourite</button>
               <button className="cart-btn">Add to cart</button>
             </div>
