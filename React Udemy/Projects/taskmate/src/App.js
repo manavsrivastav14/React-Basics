8ae20868a80209b1529b71f7d338964b80f2391c
Manav Srivastava <manavsrivastav14@gmail.com>
2023-10-23 14:56:45 +0530

    Completing taskmate project

--- React Udemy/Projects/taskmate/src/App.js
+++ React Udemy/Projects/taskmate/src/App.js
@@ -2,13 +2,33 @@ import "./App.css";
 import Header from "./components/Header";
 import AddTask from "./components/AddTask";
 import ShowTask from "./components/ShowTask";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [taskList, setTaskList] = useState(
+    JSON.parse(localStorage.getItem("taskList")) || []
+  );
+  const [task, setTask] = useState({});
+
+  useEffect(() => {
+    localStorage.setItem("taskList", JSON.stringify(taskList));
+  }, [taskList]);
+
   return (
     <div className="App">
       <Header />
-      <AddTask />
-      <ShowTask />
+      <AddTask
+        taskList={taskList}
+        setTaskList={setTaskList}
+        task={task}
+        setTask={setTask}
+      />
+      <ShowTask
+        taskList={taskList}
+        setTaskList={setTaskList}
+        task={task}
+        setTask={setTask}
+      />
     </div>
   );
 }
--- React Udemy/Projects/taskmate/src/components/Header.js
+++ React Udemy/Projects/taskmate/src/components/Header.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../assets/logo.svg";
 // import
 export default function Header() {
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "dark"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme));
+    document.documentElement.removeAttribute("class");
+    document.documentElement.classList.add(theme);
+  }, [theme]);
+
   return (
     <>
       <header>
@@ -10,12 +20,54 @@ export default function Header() {
           <span>Taskmate</span>
         </div>
         <div className="themeSelector">
-          <span className="light"></span>
-          <span className="medium"></span>
-          <span className="dark activeTheme"></span>
-          <span className="gradientOne"></span>
-          <span className="gradientTwo"></span>
-          <span className="gradientThree"></span>
+          <span
+            className={theme === "light" ? "light activeTheme" : "light"}
+            onClick={() => {
+              setTheme("light");
+            }}
+          ></span>
+          <span
+            className={theme === "medium" ? "medium activeTheme" : "medium"}
+            onClick={() => {
+              setTheme("medium");
+            }}
+          ></span>
+          <span
+            className={theme === "dark" ? "dark activeTheme" : "dark"}
+            onClick={() => {
+              setTheme("dark");
+            }}
+          ></span>
+          <span
+            className={
+              theme === "gradientOne"
+                ? "gradientOne activeTheme"
+                : "gradientOne"
+            }
+            onClick={() => {
+              setTheme("gradientOne");
+            }}
+          ></span>
+          <span
+            className={
+              theme === "gradientTwo"
+                ? "gradientTwo activeTheme"
+                : "gradientTwo"
+            }
+            onClick={() => {
+              setTheme("gradientTwo");
+            }}
+          ></span>
+          <span
+            className={
+              theme === "gradientThree"
+                ? "gradientThree activeTheme"
+                : "gradientThree"
+            }
+            onClick={() => {
+              setTheme("gradientThree");
+            }}
+          ></span>
         </div>
       </header>
     </>
